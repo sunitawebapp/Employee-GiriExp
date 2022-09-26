661f1c5b4baf55a7654690aede52be3786c03b7a
sunitawebapp <sunitachowdhury.webapp@gmail.com>
2022-09-27 03:13:40 +0530

    updated 27.09.2022

--- app/src/main/java/com/sunitawebapp/employee_giriexp/ui/fragments/SplashFragment.kt
+++ app/src/main/java/com/sunitawebapp/employee_giriexp/ui/fragments/SplashFragment.kt
@@ -1,15 +1,19 @@
 package com.sunitawebapp.employee_giriexp.ui.fragments
 
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.sunitawebapp.employee_giriexp.R
+import com.sunitawebapp.employee_giriexp.databinding.FragmentSplashBinding
 
 
 class SplashFragment : Fragment() {
-
+    lateinit var binding : FragmentSplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,10 +24,21 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        binding=FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.splashVersion.setText("Version " + com.sunitawebapp.employee_giriexp.BuildConfig.VERSION_CODE)
+
+
+
+        val secondsDelayed = 1
+        Handler().postDelayed({
+
+            findNavController().navigate(R.id.loginFragment)
+
+        }, (secondsDelayed * 2000).toLong())
         super.onViewCreated(view, savedInstanceState)
     }
 
